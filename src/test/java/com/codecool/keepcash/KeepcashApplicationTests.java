337abce592313bd6b98655089ca9f91e8e296645
kubenzi <60842782+kubenzi@users.noreply.github.com>
2020-11-28 14:46:44 +0100

    Initial commit

---
+++ src/test/java/com/codecool/keepcash/KeepcashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.keepcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepcashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
