f2d330632715f62f8c50ff8073c8d9f4fe5ec947
kubenzi <60842782+kubenzi@users.noreply.github.com>
2021-02-10 01:00:56 +0100

    Register form validation. Password and confirm password to do.

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -11,38 +11,78 @@ import {Router} from '@angular/router';
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
-  unamePattern = "^[a-z0-9_-]{8,15}$";
+  unamePattern = "^[a-z0-9_-]{5,15}$";
+  flnamePattern = "^[a-z0-9_-]{2,100}$";
+  passwordPattern = "^[a-z0-9_-]{6,12}$"; //toImprove
   email = new FormControl('', [Validators.required, Validators.email]);
+  username = new FormControl('', [Validators.required, Validators.pattern(this.unamePattern)]);
+  password = new FormControl('');
+  confirmPassword = new FormControl('');
+  firstName = new FormControl('', [Validators.required, Validators.pattern(this.flnamePattern)]);
+  lastName = new FormControl('', [Validators.required, Validators.pattern(this.flnamePattern)]);
 
   constructor(private authService: AuthService, private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-       username: ['', Validators.pattern(this.unamePattern)],
-       password: [''],
+       username: this.username,
+       password: this.password,
        email: this.email,
-       firstName: [''],
-       lastName: ['']
+       firstName: this.firstName,
+       lastName: this.lastName,
+       confirmPassword: this.confirmPassword,
     });
   }
 
   get form() { return this.registerForm.controls; }
 
-  getErrorMessage() {
+  getErrorMessageEmail() {
     if (this.email.hasError('required')) {
-      return 'You must enter a value';
+      return 'You must enter an e-mail';
     }
-
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
-  get username() {
-    return this.registerForm.get('username');
+  getErrorMessageUsername() {
+    if (this.username.hasError('required')) {
+      return 'You must enter an username';
+    }
+    return this.username.hasError('pattern') ? 'Username length range: 5 - 15' : '';
   }
 
-  get firstName() {
-    return this.registerForm.get('firstName');
+  getErrorMessagePassword() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a password';
+    }
+    return this.password.hasError('pattern') ? 'Password length range: 6 - 12' : '';
   }
+
+  getErrorMessageConfirmPassword() {
+    if (this.confirmPassword.hasError('required')) {
+      return 'Confirm password';
+    }
+    return this.registerForm.get(['password']).value != this.registerForm.get(['confirmPassword']).value ? 'Password do not match' : '';
+  }
+
+  getErrorMessageFirstName() {
+    if (this.username.hasError('required')) {
+      return 'You must enter first name';
+    }
+  }
+
+  getErrorMessageLastName() {
+    if (this.username.hasError('required')) {
+      return 'You must enter last name';
+    }
+  }
+
+  // checkPasswords(group: FormGroup) { // here we have the 'passwords' group
+  //   const password = group.get('password').value;
+  //   const confirmPassword = group.get('confirmPassword').value;
+  //
+  //   return password === confirmPassword ? null : { notSame: true }
+  // }
+
 
   register() {
     this.authService.register(
